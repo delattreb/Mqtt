b3b3134bec19741e7719e5bedb616d94c087c319
Bruno DELATTRE <delattreb@gmail.com>
2018-05-31 10:21:52 +0200

    Add testMQTT branch

--- mosca.js
+++ mosca.js
@@ -1,22 +1,25 @@
 var mosca = require('mosca');
 var mysql = require('mysql');
 
+const LOG = true;
+const INFO = false;
+
 var settings = {
     port: 1883,
     persistence: mosca.persistence.Memory
 };
 
 var connection = mysql.createConnection({
-    host: "192.168.56.103",
-    port: "3307",
-    user: "root",
-    password: "root",
-    database: "mqtt"
+    host: 'mycube.dscloud.me',
+    user: 'usermqtt',
+    password: 'infected',
+    database: 'mqtt',
+    port: 3307
 });
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("Database Connected!");
+    console.log('Database Connected!');
 });
 
 var server = new mosca.Server(settings, function () {
@@ -27,25 +30,25 @@ server.published = function (packet, client, cb) {
     if (packet.topic.indexOf('echo') === 0) {
         return cb();
     }
+    if (LOG) console.log('LOG:', packet);
 
     var newPacket = {
         topic: packet.topic,
         payload: packet.payload,
     };
-    if (packet.topic.indexOf('Temperature') === 0) {
-        // Copy payload data
-
-        insert_message(packet.payload);
-        console.log('newPacket', newPacket);
+    if (packet.topic.indexOf('iot:') === 0) {
+        var substr = packet.topic.split(':')[1];
+        insert_message(substr, packet.payload);
+        if (INFO) console.log('TOPIC:', newPacket);
     }
 };
 
-function insert_message(message) {
-    var sql = "INSERT INTO ?? (??, ??, ??) VALUES (?, ?, NOW())";
-    var params = ['datast', 'temperature', 'humidite', 'date', message, message];
+function insert_message(name, message) {
+    var sql = 'INSERT INTO ?? (??, ??, ??) VALUES (?, ?, NOW())';
+    var params = ['data', 'name', 'value', 'date', name, message];
     sql = mysql.format(sql, params);
     connection.query(sql, function (error, results) {
         if (error) throw error;
-        console.log("1 record inserted");
+        console.log('1 record inserted');
     });
-};
+}
--- publisher.js
+++ publisher.js
@@ -1,8 +1,6 @@
 var mqtt = require('mqtt');
-var client  = mqtt.connect('mqtt://127.0.0.1');
+var client = mqtt.connect('mqtt://127.0.0.1');
 client.on('connect', function () {
-       client.publish('Temperature', 'T19.87:H65.0');
-
-        console.log('Message Sent');
-
+    client.publish('iot:t1', '25.60');
+    console.log('Message Sent');
 });
