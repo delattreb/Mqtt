c2c4fb13d07b8d3de2bc69816ff3694368e9f477
Bruno DELATTRE <delattreb@gmail.com>
2018-06-22 21:57:28 +0200

    Update refactor

--- publisherTest.js
+++ publisherTest.js
@@ -2,7 +2,7 @@ let mqtt = require('mqtt');
 let client = mqtt.connect('mqtt://192.169.1.46');
 
 client.on('connect', function () {
-    client.publish('iot:ventilation', '0');
+    client.publish('ventilation', '0');
     console.log('Message Sent');
 //    process.exit();
 });
