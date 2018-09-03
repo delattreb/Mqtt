c8310a5218ca5477d67f4c861e9331fda11160fb
Bruno DELATTRE <delattreb@gmail.com>
2018-09-03 12:09:26 +0200

    Indentation

--- regulator.js
+++ regulator.js
@@ -78,7 +78,7 @@ clientMqtt.on('message', (topic, message) => {
             last_hum = hum;
             if (hum >= threshold) {
                 clientMqtt.publish(topic_ven, '1');
-                if(!bthreshold)
+                if (!bthreshold)
                     AddRegulation('Regulation On', dateFormat(new Date(), "yyyy-mm-dd H:MM:ss"), ESP_NAME, true);
                 bthreshold = true;
             } else {
