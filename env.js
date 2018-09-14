db612d959383dbb75e9ecc23117691a8378aa327
Bruno DELATTRE <delattreb@gmail.com>
2018-09-14 14:46:11 +0200

    Refactor regulator with parameter

--- env.js
+++ env.js
@@ -5,7 +5,7 @@ let server = require('mosca');
 
 module.exports = {
     // Log Level Configuration
-    loglevel: log.levels.INFO,
+    loglevel: log.levels.DEBUG,
 
     //Database configuration
     db: {
@@ -20,5 +20,13 @@ module.exports = {
     mosca: {
         port: 1883,
         persistence: server.persistence.Memory
-    }
+    },
+
+    // regulation configuration
+    address: 'mqtt://mycube.dscloud.me',
+    topic_hum: 'iot:h1',
+    topic_ven: 'ventilation',
+    topic_ven_force: 'ventilation_force',
+    location: 'Cave',
+    ESP_NAME: 'ESP Extracteur'
 }
