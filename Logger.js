0ef4e3a47d73a869d46e950a7c389037f28db5f3
Bruno DELATTRE <delattreb@gmail.com>
2018-09-14 10:42:25 +0200

    Delete personnal logger

--- Logger.js
+++
@@ -1,8 +0,0 @@
-
-
-let dateFormat = require('dateformat');
-module.exports = {
-    loginfo(level, domaine, message) {
-        console.log(level, '-', 'Domaine:', domaine, 'Message:', message.toString(), "-", dateFormat(new Date(), "dd/mm/yyyy H:MM:ss"));
-    }
-}
