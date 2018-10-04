4ff8cd1ba3cc209fc06a2a99532115b61bb9d52a
Bruno DELATTRE <delattreb@gmail.com>
2018-10-04 11:41:30 +0200

    Identation

--- regulator.js
+++ regulator.js
@@ -27,29 +27,29 @@ var options = {
 log.setDefaultLevel(env.loglevel);
 let connection;
 
-let clientMqtt = mqtt.connect(credential.address, options);
+let clientMqtt = mqtt.connect(credential.address, options); //ou a la place d'options { username: 'matteo', password: 'collina' }
 connection = mysql.createConnection(credential.db);
 
 //
 // Connection
 //
-let promiseMqtt = new Promise(function (resolve, reject) {
+let promiseMqtt = new Promise(function(resolve, reject) {
     clientMqtt.subscribe(env.topic_hum);
     clientMqtt.subscribe(env.topic_ven_force);
-    clientMqtt.on('connect', function () {
+    clientMqtt.on('connect', function() {
         log.info(dateFormat(new Date(), env.date_format), 'Connected to:', credential.address);
         resolve();
     });
 });
 
-let promiseMySQL = new Promise(function (resolve, reject) {
-    connection.connect(function () {
+let promiseMySQL = new Promise(function(resolve, reject) {
+    connection.connect(function() {
         log.info(dateFormat(new Date(), env.date_format), 'Database Connected');
         resolve();
     });
 });
 
-Promise.all([promiseMqtt, promiseMySQL]).then(function (values) {
+Promise.all([promiseMqtt, promiseMySQL]).then(function(values) {
     refreshData();
 }
 );
@@ -134,7 +134,7 @@ function getgap() {
 
 function procsql(reqsql, params) {
     sql = mysql.format(reqsql, params);
-    connection.query(sql, function (error, results) {
+    connection.query(sql, function(error, results) {
         if (error) {
             log.error(dateFormat(new Date(), env.date_format), 'MySQL connection error');
         }
