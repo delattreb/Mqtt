07821e69019d5855134bb58f6ba555ca59de485c
Bruno DELATTRE <delattreb@gmail.com>
2018-10-20 16:04:00 +0200

    Refactor

--- regulator.js
+++ regulator.js
@@ -1,104 +1,104 @@
-let mqtt = require('mqtt');
-let mysql = require('mysql');
-let dateFormat = require('dateformat');
-let log = require('loglevel');
-let env = require('./env');
-let credential = require('./credentials');
+let mqtt = require('mqtt')
+let mysql = require('mysql')
+let dateFormat = require('dateformat')
+let log = require('loglevel')
+let env = require('./env')
+let credential = require('./credentials')
 
-let threshold = 100;
-let gap = 0;
-let last_hum = 0;
-let bthreshold = false;
-let bventilation_force = false;
+let threshold = 100
+let gap = 0
+let last_hum = 0
+let bthreshold = false
+let bventilation_force = false
 var options = {
     port: 1883,
     clientId: 'Regulator'
-};
+}
 
-log.setDefaultLevel(env.loglevel);
-let connection;
+log.setDefaultLevel(env.loglevel)
+let connection
 
-let clientMqtt = mqtt.connect(credential.address, options);
-connection = mysql.createConnection(credential.db);
+let clientMqtt = mqtt.connect(credential.address, options)
+connection = mysql.createConnection(credential.db)
 
 //
 // Connection
 //
 let promiseMqtt = new Promise(function (resolve, reject) {
-    clientMqtt.subscribe(env.topic_hum);
-    clientMqtt.subscribe(env.topic_ven_force);
+    clientMqtt.subscribe(env.topic_hum)
+    clientMqtt.subscribe(env.topic_ven_force)
     clientMqtt.on('connect', function () {
-        log.info(dateFormat(new Date(), env.date_format), 'Connected to:', credential.address);
-        resolve();
-    });
-});
+        log.info(dateFormat(new Date(), env.date_format), 'Connected to:', credential.address)
+        resolve()
+    })
+})
 
 let promiseMySQL = new Promise(function (resolve, reject) {
     connection.connect(function () {
-        log.info(dateFormat(new Date(), env.date_format), 'Database Connected');
-        resolve();
-    });
-});
+        log.info(dateFormat(new Date(), env.date_format), 'Database Connected')
+        resolve()
+    })
+})
 
 Promise.all([promiseMqtt, promiseMySQL]).then(function (values) {
-    refreshData();
+    refreshData()
 }
-);
+)
 
 // --------------------------------------------------------------------------------------------------------------------
 function refreshData() {
-    getthreshold();
-    getgap();
-    log.debug(dateFormat(new Date(), env.date_format), 'Threshold:', threshold);
-    log.debug(dateFormat(new Date(), env.date_format), 'Gap:', gap);
+    getthreshold()
+    getgap()
+    log.debug(dateFormat(new Date(), env.date_format), 'Threshold:', threshold)
+    log.debug(dateFormat(new Date(), env.date_format), 'Gap:', gap)
 }
 
 //
 // MQTT
 //
 clientMqtt.on('message', (topic, message) => {
-    refreshData();
-    log.debug(dateFormat(new Date(), env.date_format), 'Message from:', topic);
-    log.debug(dateFormat(new Date(), env.date_format), 'Msg:', message.toString());
+    refreshData()
+    log.debug(dateFormat(new Date(), env.date_format), 'Message from:', topic)
+    log.debug(dateFormat(new Date(), env.date_format), 'Msg:', message.toString())
     //if (bventilation_force === false) {
-        if (topic.indexOf('iot:') === 0) {
-            let hum = parseFloat(message.toString());
-            last_hum = hum;
-            if (hum >= threshold) {
-                clientMqtt.publish(env.topic_ven, '1');
-                if (!bthreshold)
-                    AddRegulation('Regulation On', dateFormat(new Date(), mysql_date), env.ESP_NAME, true);
-                log.info(dateFormat(new Date(), env.date_format), 'Regulation On');
-                bthreshold = true;
-            } else {
-                if (bthreshold) {
-                    if (hum <= (threshold - gap)) {
-                        clientMqtt.publish(env.topic_ven, '0');
-                        AddRegulation('Regulation Off', dateFormat(new Date(), mysql_date), env.ESP_NAME, false);
-                        log.info(dateFormat(new Date(), env.date_format), 'Regulation Off');
-                        bthreshold = false;
-                    }
+    if (topic.indexOf('iot:') === 0) {
+        let hum = parseFloat(message.toString())
+        last_hum = hum
+        if (hum >= threshold) {
+            clientMqtt.publish(env.topic_ven, JSON.stringify({ value: '1' }))
+            if (!bthreshold)
+                AddRegulation('Regulation On', dateFormat(new Date(), mysql_date), env.ESP_NAME, true)
+            log.info(dateFormat(new Date(), env.date_format), 'Regulation On')
+            bthreshold = true
+        } else {
+            if (bthreshold) {
+                if (hum <= (threshold - gap)) {
+                    clientMqtt.publish(env.topic_ven, JSON.stringify({ value: '0' }))
+                    AddRegulation('Regulation Off', dateFormat(new Date(), mysql_date), env.ESP_NAME, false)
+                    log.info(dateFormat(new Date(), env.date_format), 'Regulation Off')
+                    bthreshold = false
                 }
             }
         }
+    }
     //}
     /*
     if (topic.indexOf(env.topic_ven_force) === 0) {
-        let state = parseFloat(message.toString());
+        let state = parseFloat(message.toString()) 
         if (state === 0) {
-            clientMqtt.publish(env.topic_ven, '0');
-            AddRegulation('Regulation Off', dateFormat(new Date(), "yyyy-mm-dd H:MM:ss"), env.ESP_NAME, false);
-            log.info(dateFormat(new Date(), env.date_format), 'Regulation force Off');
-            bventilation_force = false;
+            clientMqtt.publish(env.topic_ven,  JSON.stringify({ value: '0' })) 
+            AddRegulation('Regulation Off', dateFormat(new Date(), "yyyy-mm-dd H:MM:ss"), env.ESP_NAME, false) 
+            log.info(dateFormat(new Date(), env.date_format), 'Regulation force Off') 
+            bventilation_force = false 
         }
         else {
-            clientMqtt.publish(env.topic_ven, '1');
-            AddRegulation('Regulation On', dateFormat(new Date(), "yyyy-mm-dd H:MM:ss"), env.ESP_NAME, true);
-            log.info(dateFormat(new Date(), env.date_format), 'Regulation force On');
-            bventilation_force = true;
+            clientMqtt.publish(env.topic_ven,  JSON.stringify({ value: '1' })) 
+            AddRegulation('Regulation On', dateFormat(new Date(), "yyyy-mm-dd H:MM:ss"), env.ESP_NAME, true) 
+            log.info(dateFormat(new Date(), env.date_format), 'Regulation force On') 
+            bventilation_force = true 
         }
     }*/
-});
+})
 //---------------------------------------------------------------------------------------------------------------------
 
 
@@ -106,33 +106,33 @@ clientMqtt.on('message', (topic, message) => {
 // MySQL
 //
 function AddRegulation(tag, date, name, state) {
-    let sqlESPConnect = 'INSERT INTO regulation (tag, date, name, state) VALUES (?, ?, ?, ?)';
-    let params = [tag, date, name, state];
-    procsql(sqlESPConnect, params);
-    log.info(name, 'Insert regulation', state);
+    let sqlESPConnect = 'INSERT INTO regulation (tag, date, name, state) VALUES (?, ?, ?, ?)'
+    let params = [tag, date, name, state]
+    procsql(sqlESPConnect, params)
+    log.info(name, 'Insert regulation', state)
 }
 
 function getthreshold() {
-    let sql = 'SELECT threshold FROM configuration WHERE name=? LIMIT 1';
-    let params = [env.location];
-    threshold = procsql(sql, params);
+    let sql = 'SELECT threshold FROM configuration WHERE name=? LIMIT 1'
+    let params = [env.location]
+    threshold = procsql(sql, params)
 }
 
 function getgap() {
-    let sql = 'SELECT gap FROM configuration WHERE name=? LIMIT 1';
-    let params = [env.location];
-    gap = procsql(sql, params);
+    let sql = 'SELECT gap FROM configuration WHERE name=? LIMIT 1'
+    let params = [env.location]
+    gap = procsql(sql, params)
 }
 
 function procsql(reqsql, params) {
-    sql = mysql.format(reqsql, params);
+    sql = mysql.format(reqsql, params)
     connection.query(sql, function (error, results) {
         if (error) {
-            log.error(dateFormat(new Date(), env.date_format), 'MySQL connection error');
+            log.error(dateFormat(new Date(), env.date_format), 'MySQL connection error')
         }
-        log.debug(dateFormat(new Date(), env.date_format), results);
-        return parseFloat(results[0].gap);
-    });
+        log.debug(dateFormat(new Date(), env.date_format), results)
+        return parseFloat(results[0].gap)
+    })
 }
 
 //--------------------------------------------------------------------------------------------------------------------
--- server.js
+++ server.js
@@ -1,80 +1,80 @@
-let mysql = require('mysql');
-let mosca = require('mosca');
-let dateFormat = require('dateformat');
-let log = require('loglevel');
-let env = require('./env');
-let credential = require('./credentials');
-let connection;
+let mysql = require('mysql') 
+let mosca = require('mosca') 
+let dateFormat = require('dateformat') 
+let log = require('loglevel') 
+let env = require('./env') 
+let credential = require('./credentials') 
+let connection 
 
-log.setDefaultLevel(env.loglevel);
+log.setDefaultLevel(env.loglevel) 
 
 // MySQL
-connection = mysql.createConnection(credential.db);
+connection = mysql.createConnection(credential.db) 
 
 connection.connect(function () {
-    log.info(dateFormat(new Date(), env.date_format), 'Database Connected');
-});
+    log.info(dateFormat(new Date(), env.date_format), 'Database Connected') 
+}) 
 
 function updateESPConnected(name, state) {
-    let reqsql = 'UPDATE esp SET connected=? WHERE name=?';
-    let params = [state, name];
-    procsql(reqsql, params);
+    let reqsql = 'UPDATE esp SET connected=? WHERE name=?' 
+    let params = [state, name] 
+    procsql(reqsql, params) 
 }
 
 function updateESPState(name, state) {
-    let reqsql = 'UPDATE esp SET state=? WHERE name=?';
-    let params = [state, name];
-    procsql(reqsql, params);
+    let reqsql = 'UPDATE esp SET state=? WHERE name=?' 
+    let params = [state, name] 
+    procsql(reqsql, params) 
 }
 
 function insert_message(name, message) {
-    let reqsql = 'INSERT INTO data (??, ??, ??) VALUES (?, ?, NOW())';
-    let params = ['name', 'value', 'date', name, message];
-    procsql(reqsql, params);
+    let reqsql = 'INSERT INTO data (??, ??, ??) VALUES (?, ?, NOW())' 
+    let params = ['name', 'value', 'date', name, message] 
+    procsql(reqsql, params) 
 }
 
 function procsql(reqsql, params) {
-    sql = mysql.format(reqsql, params);
+    sql = mysql.format(reqsql, params) 
     connection.query(sql, function (error, results) {
         if (error) {
-            //throw error;
-            log.error(dateFormat(new Date(), env.date_format), 'MySQL connection error');
+            //throw error 
+            log.error(dateFormat(new Date(), env.date_format), 'MySQL connection error') 
         }
-        //log.debug(dateFormat(new Date(), env.date_format), results);
-    });
+        //log.debug(dateFormat(new Date(), env.date_format), results) 
+    }) 
 }
 // Start program
 mosca = new mosca.Server(env.mosca, function () {
-});
+}) 
 
 mosca.on('ready', function () {
-    log.info(dateFormat(new Date(), env.date_format), 'Mosca server is up and running');
-});
+    log.info(dateFormat(new Date(), env.date_format), 'Mosca server is up and running') 
+}) 
 mosca.on('subscribed', function (topic, client) {
-    log.info(dateFormat(new Date(), env.date_format), 'Subscribed', client.id, topic);
-});
+    log.info(dateFormat(new Date(), env.date_format), 'Subscribed', client.id, topic) 
+}) 
 mosca.on('unsubscribed', function (topic, client) {
-    log.info(dateFormat(new Date(), env.date_format), 'Unsubscribed', client.id, topic);
-});
+    log.info(dateFormat(new Date(), env.date_format), 'Unsubscribed', client.id, topic) 
+}) 
 mosca.on('clientConnected', function (client) {
-    log.info(dateFormat(new Date(), env.date_format), 'Connected', client.id);
-    updateESPConnected(client.id, true);
-});
+    log.info(dateFormat(new Date(), env.date_format), 'Connected', client.id) 
+    updateESPConnected(client.id, true) 
+}) 
 mosca.on('clientDisconnected', function (client) {
-    log.info(dateFormat(new Date(), env.date_format), 'Disconnected', client.id);
-    updateESPConnected(client.id, false);
-});
-mosca.on('published', publish);
+    log.info(dateFormat(new Date(), env.date_format), 'Disconnected', client.id) 
+    updateESPConnected(client.id, false) 
+}) 
+mosca.on('published', publish) 
 function publish(packet, client, cb) {
     if (packet.topic.indexOf('iot:') === 0) {
-        log.debug(dateFormat(new Date(), env.date_format), 'client', client.id, 'pub', packet.topic.split(':')[1], 'value', packet.payload.toString());
-        let substr = packet.topic.split(':')[1];
-        insert_message(substr, packet.payload);
+        log.debug(dateFormat(new Date(), env.date_format), 'client', client.id, 'pub', packet.topic.split(':')[1], 'value', packet.payload.toString()) 
+        let substr = packet.topic.split(':')[1] 
+        insert_message(substr, packet.payload) 
     }
     if (packet.topic.indexOf(env.topic_ven) === 0) {
-        log.debug(dateFormat(new Date(), env.date_format), 'client', client.id, 'Set topic', env.topic_ven, 'To', JSON.parse(packet.payload).value);
-        let bstate = parseInt(JSON.parse(packet.payload).jacket);
-        updateESPState('ESP Extracteur 1', bstate);
-        updateESPState('ESP Extracteur 2', bstate);
+        log.debug(dateFormat(new Date(), env.date_format), 'client', client.id, 'Set topic', env.topic_ven, 'To', JSON.parse(packet.payload).value) 
+        let bstate = parseInt(JSON.parse(packet.payload).jacket) 
+        updateESPState('ESP Extracteur 1', bstate) 
+        updateESPState('ESP Extracteur 2', bstate) 
     }
 }
