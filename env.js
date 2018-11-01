d306d860a6a43aab7aa93490607ba86fece2604e
Bruno DELATTRE <delattreb@gmail.com>
2018-11-01 09:26:44 +0100

    Fix

--- env.js
+++ env.js
@@ -4,18 +4,17 @@ let server = require('mosca');
 
 module.exports = {
     // Log Level Configuration
-    mqttport : 1883,
     loglevel: log.levels.INFO,
 
     // Mosca Server configuration
     mosca: {
-        port: this.mqttport,
+        port: 1883,
         persistence: server.persistence.Memory
     },
     
     // MQTT regulation
     mqttoptions: {
-        port: this.mqttport,
+        port: 1883,
         clientId: 'Regulator'
     },
 
