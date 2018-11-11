e9174b8e35ffc561884aee3d0872a9b4a74245cc
Bruno DELATTRE <delattreb@gmail.com>
2018-11-11 11:23:22 +0100

    Fix

--- up.sh
+++ up.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+echo "Stop services"
 forever stop server.js
 forever stop regulator.js
 forever stop app.js
+
+echo "Start API services"
+NODE_ENV=production forever start -e err_api.log /home/dietpi/APIMQTT/app.js
+echo "Wait 10s"
+sleep 10
 forever start -o server.log -e err_server.log server.js
 forever start -o regulator.log -e err_regulator.log regulator.js
-sleep 10
-NODE_ENV=production forever start -e err_api.log app.js
