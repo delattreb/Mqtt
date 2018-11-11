a2dabe707b6ca67235b82c6614c251f0aebb3a48
Bruno DELATTRE <delattreb@gmail.com>
2018-11-11 11:20:27 +0100

    Fix

--- up.sh
+++ up.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 forever stop server.js
 forever stop regulator.js
+forever stop app.js
 forever start -o server.log -e err_server.log server.js
 forever start -o regulator.log -e err_regulator.log regulator.js
+sleep 10
+NODE_ENV=production forever start -e err_api.log app.js
